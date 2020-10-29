101e948656e8d83333c76768e95e00012e9b290e
Suraj <surajbab@thoughtworks.com>
2020-10-29 10:59:10 +0530

    Initial Spring Boot Application

---
+++ src/main/java/com/thoughtworks/dependencydemoapp/DependencyDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.dependencydemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependencyDemoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thoughtworks/dependencydemoapp/DependencyDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.dependencydemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
