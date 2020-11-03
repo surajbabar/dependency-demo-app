c9df47373bf81743516091a449bd784432eae96b
Suraj <surajbab@thoughtworks.com>
2020-11-03 11:37:55 +0530

    Sahil/Suraj | Downgrade dependencies to state where RCE is possible through post API

--- src/main/java/com/thoughtworks/dependencydemoapp/data/SampleDataController.java
+++ src/main/java/com/thoughtworks/dependencydemoapp/data/SampleDataController.java
@@ -1,26 +1,76 @@
 package com.thoughtworks.dependencydemoapp.data;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.jcip.annotations.Immutable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleDataController {
+
+    private static ObjectMapper deserializer = new ObjectMapper().enableDefaultTyping();
+    private static ObjectMapper serializer = new ObjectMapper();
+    public static final Logger log = LoggerFactory.getLogger(SampleDataController.class);
+
     @GetMapping(path = "/data")
     public Data getData() {
         return new Data("Hello World !!!!!");
     }
 
-}
+    @PostMapping(path = "data")
+    public ResponseEntity<String> updateData(@RequestBody String request) throws JsonProcessingException {
 
-class Data {
-    private String data;
-
-    Data(String data) {
-
-        this.data = data;
+        log.debug("post data: {}",request);
+        
+        Data data = deserialize(request);
+        if (data != null) {
+            return new ResponseEntity<String>(" successfully created",HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<String>("invalid content", HttpStatus.BAD_REQUEST);
+        }
     }
 
-    public String getData() {
-        return data;
+    private static Data deserialize(String request) {
+        try {
+            return deserializer.readValue(request, Data.class);
+        } catch (Exception e) {
+            log.warn("Unexpected exception deserializing content: ", e);
+            return null;
+        }
+    }
+
+
+}
+@Immutable
+class Data {
+    private Object value;
+    Data() {
+
+    }
+    Data(String value) {
+
+        this.value = value;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Data{" +
+                "value=" + value +
+                '}';
     }
 }
